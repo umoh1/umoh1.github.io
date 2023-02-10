43d6c8aa1b33a4eb238280907cda9ed4f565caea
CodeSandbox <support@codesandbox.io>
2023-02-10 05:15:44 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  
+  
+  return (
+    <>
+      <Head>
+        <title>NJ Umoh Portfolio</title>
+        <meta name="description" content="NJ Umoh Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      {/** NAV BAR */}
+      
+      {/** Hero */}
+      
+      {/** Your work experience */}
+      
+      {/** Projects */}
+      
+      {/** Contact Information*/}
+    </>
+  );
+}
